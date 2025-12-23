8bcf2fe7dca4aedeb23eede34bb3bf80f030d4ce
Emelinur <emelinuryzgc@gmail.com>
2025-12-23 17:30:03 +0300

    feat: Implement responsive design and add main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const cardDaily=document.querySelector(".daily");
+const cardWeekly=document.querySelector(".weekly");
+const cardMonthly=document.querySelector(".monthly");
+
+
+cardDaily.addEventListener("click",()=>{
+  console.log("click")
+})
+
+cardWeekly.addEventListener("click",()=>{
+  console.log("click")
+})
+
+cardMonthly.addEventListener("click",()=>{
+  console.log("click")
+})
