a702c48d8bcde04c8a9e474a019b673ae90100fc
Emelinur <emelinuryzgc@gmail.com>
2025-12-24 17:56:56 +0300

    feat: Implement timeframe switching logic and data fetching

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 const cardDaily=document.querySelector(".daily");
 const cardWeekly=document.querySelector(".weekly");
 const cardMonthly=document.querySelector(".monthly");
-
-
-cardDaily.addEventListener("click",()=>{
-  console.log("click")
-})
+const allButtons=document.querySelector(".card-link");
 
 cardWeekly.addEventListener("click",()=>{
   console.log("click")
@@ -14,3 +10,18 @@ cardWeekly.addEventListener("click",()=>{
 cardMonthly.addEventListener("click",()=>{
   console.log("click")
 })
+let jsonData=[];
+async function getData() {
+  try{
+    const response=await fetch("./data.json");
+    if (!response.ok) {
+      throw new Error('Failed to load data, status:' + response.status);
+    }
+    jsonData=await response.json()
+
+  }catch{
+    console.error("Something went wrong:",error)
+  }
+  
+}
+
